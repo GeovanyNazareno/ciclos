8642c8c369e6b1998518363023d5c04ec23bf0ef
Geovanny <nazrenocastillog@gmail.com>
2024-10-23 17:22:47 -0500

    numprimos

--- javascript/main.js
+++ javascript/main.js
@@ -37,7 +37,7 @@ for(j=0; j<=tamaño; j++){
 }*/
 
 
-var numeros = []; 
+/*var numeros = []; 
     
 for (var i = 0; i < 5; i++) {
     var num = prompt("Ingrese el número " + (i + 1) + ":");
@@ -61,3 +61,51 @@ for (var i = 0; i < numeros.length; i++) {
 }
 document.write("<br>El número mayor es: " + mayor);
 document.write("<br>El número menor es: " + menor);
+
+//Ingresamos por teclado el tamaño del arreglo
+var tamanio = parseInt(prompt("ingrese el tamaño del arreglo"))
+//Inicialisamos el tamaño del arreglo
+var arreglo = [tamanio]
+//iniciamos la variable mayor
+mayor = arreglo[0]
+//iniciamos la variable menor
+menor=arreglo[0]
+//Ingreso los datos por teclado mediante un ciclo for
+for(i=0; i<=tamanio; i++){
+    arreglo [i]=parseInt(prompt("Ingrese el numero " + (i+1)))
+
+}
+//imprmimos en pantalla los datos
+document.write(arreglo)
+
+//recorremos el arreglo
+for(i=0; itama)
+*/
+    //Un pograma en js donde el usuario
+    //ingrese un numero por teclado
+    //verificar si ese numero es primo o no
+    //un numero es primo cuando es divisible para uno y para si mismo 
+
+    //Ingresamos el tamaño del arreglo
+//var numero = prompt("ingrese el numero a verificar");
+
+//var contador = 0
+document.write("Los numeros primos son:" + "<br>")
+for(let num = 1; num<=10; num++){
+    //Creo una variable en donde me va a contar los divisores.
+   let divisor = 0;
+    //Creare una funcion que me va a ir iterando
+    //los numeros de 1 en 1 para encontrar sus divisores
+   for(let i=1; i<=num; i++){
+    if (num % i === 0){
+        divisor++;
+    }
+   }
+   //Esta funcion me ayudara a ver que el numero
+   //Tenga exactamente dos divisores
+   if (divisor === 2){
+        document.write(num + "<br>")
+   }
+
+}
+
